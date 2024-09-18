d48a0dd5d40050f0f317e535c7effbe76b4bbea5
SzaroBury <mkmateuszk@o2.pl>
2024-09-18 21:06:28 +0200

    Formatting

--- RespectCounter.Infrastructure/Repositories/Repository.cs
+++ RespectCounter.Infrastructure/Repositories/Repository.cs
@@ -1,27 +1,28 @@
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
+using RespectCounter.Domain.Model;
 using RespectCounter.Domain.Interfaces;
 
 namespace RespectCounter.Infrastructure.Repositories
 {
     public class Repository : IRepository
     {
-        private readonly RespectDbContext _dbContext;
+        private readonly RespectDbContext dbContext;
 
         public Repository(RespectDbContext dbContext)
         {
-            _dbContext = dbContext;
+            this.dbContext = dbContext;
         }
 
-        public async Task<T?> GetById<T>(int id) where T : IEntity
+        public async Task<T?> GetById<T>(int id) where T : Entity
         {
-            return await _dbContext.Set<T>().FindAsync(id);
+            return await dbContext.Set<T>().FindAsync(id);
         }
 
         public IQueryable<T> FindQueryable<T>(Expression<Func<T, bool>> expression,
-            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null) where T : IEntity
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null) where T : Entity
         {
-            var query = _dbContext.Set<T>().Where(expression);
+            var query = dbContext.Set<T>().Where(expression);
             return orderBy != null ? orderBy(query) : query;
         }
 
@@ -29,36 +30,35 @@ namespace RespectCounter.Infrastructure.Repositories
             IOrderedQueryable<T>>? orderBy = null, CancellationToken cancellationToken = default)
             where T : class
         {
-            var query = expression != null ? _dbContext.Set<T>().Where(expression) : _dbContext.Set<T>();
+            var query = expression != null ? dbContext.Set<T>().Where(expression) : dbContext.Set<T>();
             return orderBy != null
                 ? orderBy(query).ToListAsync(cancellationToken)
                 : query.ToListAsync(cancellationToken);
         }
 
-        public Task<List<T>> FindAllAsync<T>(CancellationToken cancellationToken) where T : IEntity
+        public Task<List<T>> FindAllAsync<T>(CancellationToken cancellationToken) where T : Entity
         {
-            return _dbContext.Set<T>().ToListAsync(cancellationToken);
+            return dbContext.Set<T>().ToListAsync(cancellationToken);
         }
 
-        public Task<T?> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression, string includeProperties) where T : IEntity
+        public Task<T?> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression, string includeProperties) where T : Entity
         {
-            var query = _dbContext.Set<T>().AsQueryable();
+            var query = dbContext.Set<T>().AsQueryable();
 
-            query = includeProperties.Split(new char[] { ',' }, 
-                StringSplitOptions.RemoveEmptyEntries).Aggregate(query, (current, includeProperty) 
-                => current.Include(includeProperty));
+            query = includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
 
             return query.SingleOrDefaultAsync(expression);
         }
 
-        public T Add<T>(T entity) where T : IEntity
+        public T Add<T>(T entity) where T : Entity
         {
-            return _dbContext.Set<T>().Add(entity).Entity;
+            return dbContext.Set<T>().Add(entity).Entity;
         }
 
-        public void Update<T>(T entity) where T : IEntity
+        public void Update<T>(T entity) where T : Entity
         {
-            _dbContext.Entry(entity).State = EntityState.Modified;
+            dbContext.Entry(entity).State = EntityState.Modified;
         }
 
         // public void UpdateRange<T>(IEnumerable<T> entities) where T : IEntity
@@ -66,9 +66,9 @@ namespace RespectCounter.Infrastructure.Repositories
         //     _dbContext.Set<T>().UpdateRange(entities);
         // }
 
-        public void Delete<T>(T entity) where T : IEntity
+        public void Delete<T>(T entity) where T : Entity
         {
-            _dbContext.Set<T>().Remove(entity);
+            dbContext.Set<T>().Remove(entity);
         }
     }
 }
