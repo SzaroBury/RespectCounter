baca9070384fb774d43bfc4c18de9cdee1796ea6
SzaroBury <mkmateuszk@o2.pl>
2024-09-18 21:22:44 +0200

    Added regions, implemented the GetVerifiedPersons method, added empty command methods

--- RespectCounter.API/Controllers/PersonController.cs
+++ RespectCounter.API/Controllers/PersonController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using RespectCounter.Application.Queries;
+using RespectCounter.Domain.Model;
 
 namespace RespectCounter.API.Controllers;
 
@@ -16,16 +17,17 @@ namespace RespectCounter.API.Controllers;
 //Verify a person
 //React
 //Comment
-//Propose a change
 //Add a tag
+//Propose a change
 //?Hide a person/Mark as invalid
 
 //other todos: auto added tag based on nationality
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/persons")]
 public class PersonController: ControllerBase
 {
+
     private readonly ILogger<PersonController> logger;
     private readonly ISender mediator;
 
@@ -35,27 +37,33 @@ public class PersonController: ControllerBase
         this.mediator = mediator;
     }
 
-    [HttpGet("api/persons")]
-    public async Task<IActionResult> GetPersons()
+    #region Querries
+    [HttpGet("/api/persons")]
+    public async Task<IActionResult> GetVerifiedPersons([FromQuery] string search, [FromQuery] string order)
     {
-        var query = new GetPersonsQuery();
+        var query = new GetVerifiedPersonsQuery(search, order);
+        var result = await mediator.Send(query);
+
+        return Ok(result);
+    }
+
+    [HttpGet("/api/persons/all")]
+    public async Task<IActionResult> GetPersons([FromQuery] string search, [FromQuery] string order)
+    {
+        var query = new GetPersonsQuery(search, order);
         var result = await mediator.Send(query);
 
         return Ok(result);
     }
 
     [HttpGet("{id}")]
-    public async Task<IActionResult> GetPerson(int id)
+    public async Task<IActionResult> GetPerson(string id)
     {
-        var query = new GetPersonByIdQuery()
-        { 
-            Id = id 
-        };
-        
+        var query = new GetPersonByIdQuery(id);
+
         try
         {
             var result = await mediator.Send(query);
-
             return Ok(result);
         }
         catch (KeyNotFoundException)
@@ -63,4 +71,50 @@ public class PersonController: ControllerBase
             return NotFound();
         }
     }
+
+    #endregion
+
+    #region Commands
+    [HttpPost()]
+    public Task<IActionResult> ProposePerson([FromBody] Person newPerson)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}/verify")]
+    public Task<IActionResult> VerifyPerson(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("{id}/respect/{reaction}")]
+    public Task<IActionResult> RespectPerson(string id, int reaction)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("{id}/comment")]
+    public Task<IActionResult> CommentPerson(string id, [FromBody] Comment newComment)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost("{id}/tag/{tags}")]
+    public Task<IActionResult> TagPerson(string id, string tags)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}")]
+    public Task<IActionResult> ProposeUpdatePerson(string id, [FromBody] Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}/hide")]
+    public Task<IActionResult> HidePerson(string id)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
 }
