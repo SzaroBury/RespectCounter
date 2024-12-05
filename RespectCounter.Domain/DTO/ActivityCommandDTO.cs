ece50b476926334cf2a5a62025c9209cc0b8f98f
SzaroBury <mkmateuszk@o2.pl>
2024-12-05 15:57:03 +0100

    Added missing Persons field

--- RespectCounter.Domain/DTO/ActivityCommandDTO.cs
+++ RespectCounter.Domain/DTO/ActivityCommandDTO.cs
@@ -1,11 +1,12 @@
 namespace RespectCounter.Domain.DTO;
 
 public record ActivityCommandDTO(
-         string Value, 
-         string Description, 
-         string Location, 
-         string Happend, 
-         string Source, 
-         int Type, 
-         string Tags
+    string Persons,
+    string Value, 
+    string Description, 
+    string Location, 
+    string Happend, 
+    string Source, 
+    int Type, 
+    string Tags
 );
