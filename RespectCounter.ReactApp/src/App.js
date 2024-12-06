be419639e851ef020ab8763c9b4afb9396a9c37b
SzaroBury <mkmateuszk@o2.pl>
2024-12-06 11:44:51 +0100

    Added LoginPopup

--- RespectCounter.ReactApp/src/App.js
+++ RespectCounter.ReactApp/src/App.js
@@ -1,16 +1,38 @@
 import { BrowserRouter as Router} from 'react-router-dom';
-import Container from 'react-bootstrap/Container'
+import { useState } from 'react';
 import PageRenderer from './utils/PageRenderer/PageRenderer';
 import Header from './components/Header/Header';
+import LoginPopup from './components/LoginPopup/LoginPopup';
+import './App.css'
 
 function App(props) {
+    const [showLoginPopup, setShowLoginPopup] = useState(false);
+    const [userLogged, setUserLogged] = useState(false);
+    const [userName, setUserName] = useState("");
+    const handleOpenLoginPopup = () => setShowLoginPopup(true);
+    const handleCloseLoginPopup = () => setShowLoginPopup(false);
+    const handleUserLoggedIn = (username) => { 
+        setUserLogged(true);
+        setUserName(username);
+        console.log(`App: User logged in: ${username}`); 
+    }
+    const handleUserLoggedOut = () => { 
+        setUserLogged(false);
+        setUserName("");
+        console.log("App: User logged out"); 
+    }
+
     return ( 
-        <Container className="p-3">
-            <Header/>
+        <div className='app'>
+            <Header onLoginClick={handleOpenLoginPopup} onLogoutClick={handleUserLoggedOut} logged={userLogged} user={userName}/>
             <Router>
                 <PageRenderer/>
             </Router>
-        </Container>
+            <div className='footer'>
+                <div>Made by Mateusz Kubicki</div>
+            </div>
+            {showLoginPopup && <LoginPopup onCancel={handleCloseLoginPopup} onSuccessfullLoggingIn={handleUserLoggedIn}/>}
+        </div>
     );
 };
 
--- RespectCounter.ReactApp/src/components/Header/Header.js
+++ RespectCounter.ReactApp/src/components/Header/Header.js
@@ -1,33 +1,56 @@
-function Header(props) {
+import { useState } from 'react';
+import "./Header.css"
+
+function Header({onLoginClick, onLogoutClick, logged, user}) {
+
     return (
-        <div class="m-3 d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-            {/*<svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>*/}
-            <span class="fs-4">Public Figures</span>
-        </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><a href="/" class="nav-link px-2 link-dark">Home</a></li>
-            <li><a href="/weather" class="nav-link px-2 link-dark">Weather</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control" placeholder="Search..." aria-label="Search"/ >
-        </form>
-
-        <div class="dropdown text-end">
-            <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" class="rounded-circle"/ >
+        <div className="header">
+            <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                {/*<svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>*/}
+                <span className="fs-4">Respect Counter</span>
             </a>
-            <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                <li><a className="dropdown-item" href="#">New project...</a></li>
-                <li><a className="dropdown-item" href="#">Settings</a></li>
-                <li><a className="dropdown-item" href="#">Profile</a></li>
-                <li><hr className="dropdown-divider"/></li>
-                <li><a className="dropdown-item" href="#">Sign out</a></li>
+
+            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="/events" className="nav-link link-dark">Events</a></li>
+                <li><a href="/persons" className="nav-link link-dark">Persons</a></li>
+                <li><a href="/about" className="nav-link link-dark">About</a></li>
             </ul>
+
+            {/* <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input type="search" className="form-control" placeholder="Search..." aria-label="Search"/ >
+            </form> */}
+
+            <div className="d-flex align-items-center gap-3">
+                <span><i className="bi bi-moon-stars-fill"></i></span>
+                {/* <div className="dropdown text-end">
+                    <a className="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle"/>
+                    </a>
+                    <ul className="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+                        <li><a className="dropdown-item" href="/#">New project...</a></li>
+                        <li><a className="dropdown-item" href="/#">Settings</a></li>
+                        <li><a className="dropdown-item" href="/#">Profile</a></li>
+                        <li><hr className="dropdown-divider"/></li>
+                        <li><a className="dropdown-item" href="/#">Sign out</a></li>
+                    </ul>
+                </div> */}
+
+                {
+                    logged ? (
+                        <div>
+                            <span className='m-3'>Hi {user}!</span>
+                            <button className='btn btn-secondary' onClick={onLogoutClick}>Log out</button>    
+                        </div>  
+                        
+                    ) : (
+                        <div>
+                            <button className='btn btn-secondary' onClick={onLoginClick}>Log in</button>
+                        </div>
+                    )
+                }
+            </div>
+            
         </div>
-      </div>
     );
 };
 
