ee11238f8babcb294870b23466b547ab122570d0
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 23:52:13 +0100

    Added and reordered new pages.

--- RespectCounter.ReactApp/src/utils/PageRenderer/PageRenderer.js
+++ RespectCounter.ReactApp/src/utils/PageRenderer/PageRenderer.js
@@ -1,22 +1,34 @@
+import "./PageRenderer.css";
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import AboutPage from '../../Pages/AboutPage/AboutPage';
 import HomePage from '../../Pages/HomePage/HomePage';
-import ActivitiesPage from '../../Pages/ActivitiesPage/ActivitiesPage';
-import PersonsPage from '../../Pages/PersonsPage/PersonsPage';
+import AboutPage from '../../Pages/AboutPage/AboutPage';
 import SignInPage from '../../Pages/SignInPage/SignInPage';
+import ActivitiesPage from '../../Pages/ActivitiesPage/ActivitiesPage';
+import ActivityDetailsPage from '../../Pages/ActivitiesPage/ActivityDetailsPage/ActivityDetailsPage';
+import CreateActivityPage from "../../Pages/ActivitiesPage/CreateActivityPage/CreateActivityPage";
+import PersonsPage from '../../Pages/PersonsPage/PersonsPage';
+import CreatePersonPage from "../../Pages/PersonsPage/CreatePersonPage/CreatePersonPage";
+import PersonDetailsPage from "../../Pages/PersonsPage/PersonDetailsPage/PersonDetailsPage";
+import UnknownPage from "../../Pages/UnknownPage/UnknownPage";
+import AdminPage from "../../Pages/AdminPage/AdminPage";
 
-import "./PageRenderer.css";
 
 const PageRenderer = () => {
     return (
         <div className='page'>
             <Routes>
                 <Route exact path="/" element={<HomePage /> } />
-                <Route exact path="/events" element={<ActivitiesPage /> } />
+                <Route exact path="/activities" element={<ActivitiesPage /> } />
+                <Route exact path="/act/:id" element={<ActivityDetailsPage />} />
+                <Route exact path="/activity/create" element={<CreateActivityPage /> } />
                 <Route exact path="/persons" element={<PersonsPage /> } />
+                <Route exact path="/person/create" element={<CreatePersonPage />} />
+                <Route exact path="/person/:id" element={<PersonDetailsPage />} />
                 <Route exact path="/about" element={<AboutPage />} />
                 <Route exact path="/signin" element={<SignInPage />} />
+                <Route exact path="/admin" element={<AdminPage />} />
+                <Route exact path="*" element={<UnknownPage />} />
                 {/* <Route path="*" element={<Navigate to="/" />} /> */}
             </Routes>
         </div>
