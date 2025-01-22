166a27e335f67fdcfd9876552c8bb0f8569fb7dc
SzaroBury <mkmateuszk@o2.pl>
2025-01-23 00:19:43 +0100

    Refactored authorization logic by moving it to AuthProvider. Separated Footer into an independent component. Added LoginPopup that is displayed over the whole app when needed.

--- RespectCounter.ReactApp/src/App.js
+++ RespectCounter.ReactApp/src/App.js
@@ -1,38 +1,23 @@
-import { BrowserRouter as Router} from 'react-router-dom';
-import { useState } from 'react';
-import PageRenderer from './utils/PageRenderer/PageRenderer';
-import Header from './components/Header/Header';
-import LoginPopup from './components/LoginPopup/LoginPopup';
 import './App.css'
+import { BrowserRouter as Router} from 'react-router-dom';
+import PageRenderer from './utils/PageRenderer/PageRenderer';
+import { AuthProvider } from './utils/AuthProvider/AuthProvider';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App(props) {
-    const [showLoginPopup, setShowLoginPopup] = useState(false);
-    const [userLogged, setUserLogged] = useState(false);
-    const [userName, setUserName] = useState("");
-    const handleOpenLoginPopup = () => setShowLoginPopup(true);
-    const handleCloseLoginPopup = () => setShowLoginPopup(false);
-    const handleUserLoggedIn = (username) => { 
-        setUserLogged(true);
-        setUserName(username);
-        console.log(`App: User logged in: ${username}`); 
-    }
-    const handleUserLoggedOut = () => { 
-        setUserLogged(false);
-        setUserName("");
-        console.log("App: User logged out"); 
-    }
 
     return ( 
         <div className='app'>
-            <Header onLoginClick={handleOpenLoginPopup} onLogoutClick={handleUserLoggedOut} logged={userLogged} user={userName}/>
             <Router>
-                <PageRenderer/>
+                <AuthProvider>
+                    <Header />
+                    <PageRenderer/>
+                    <Footer/>
+                </AuthProvider>
             </Router>
-            <div className='footer'>
-                <div>Made by Mateusz Kubicki</div>
-            </div>
-            {showLoginPopup && <LoginPopup onCancel={handleCloseLoginPopup} onSuccessfullLoggingIn={handleUserLoggedIn}/>}
         </div>
+    
     );
 };
 
