a61915401d618678c1ec8e1aef5710622bd4e4a8
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 14:28:37 +0100

    Added a new query for the Tag controller

---
+++ RespectCounter.Application/Tag/Queries/GetSimpleTagsQuery.cs
@@ -0,0 +1,28 @@
+using MediatR;
+using RespectCounter.Infrastructure.Repositories;
+using RespectCounter.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using RespectCounter.Application.DTOs;
+
+namespace RespectCounter.Application.Queries
+{
+    public record GetSimpleTagsQuery() : IRequest<List<SimpleTagDTO>>;
+
+    public class GetSimpleTagsQueryHandler : IRequestHandler<GetSimpleTagsQuery, List<SimpleTagDTO>>
+    {
+        private readonly IUnitOfWork uow;
+
+        public GetSimpleTagsQueryHandler(IUnitOfWork uow)
+        {
+            this.uow = uow;
+        }
+
+        public async Task<List<SimpleTagDTO>> Handle(GetSimpleTagsQuery request, CancellationToken cancellationToken)
+        {
+            return await uow.Repository().FindQueryable<Tag>(t => t.Level > 0)
+                .OrderByDescending(t => t.Activities.Count + t.Persons.Count)
+                .Select(t => t.ToSimpleDTO())
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
