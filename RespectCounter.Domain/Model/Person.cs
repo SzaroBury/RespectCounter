bf9b9865247ea4482bac682a969fe7f72cb9bfc9
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 14:35:37 +0100

    'Birthday' field was set as nullable.

--- RespectCounter.Domain/Model/Person.cs
+++ RespectCounter.Domain/Model/Person.cs
@@ -4,10 +4,11 @@
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string NickName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Nationality { get; set; } = string.Empty;
-        public DateOnly Birthday { get; set; } = DateOnly.MinValue;
-        public DateOnly? DeathDate { get; set; } = null;
+        public DateOnly? Birthday { get; set; }
+        public DateOnly? DeathDate { get; set; }
         public PersonStatus Status { get; set; } = PersonStatus.NotVerified;
         public virtual List<Activity> Activities { get; set; } = new();
         public virtual List<Comment> Comments { get; set; } = new();
@@ -15,7 +16,7 @@
         public virtual List<Tag> Tags { get; set; } = new();
     }
 
-        public enum PersonStatus
+    public enum PersonStatus
     {
         NotVerified,
         Verified,
