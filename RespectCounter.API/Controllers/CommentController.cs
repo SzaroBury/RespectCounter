879cab05814ffcad63cf48f8a06e224e03ef78ed
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 23:03:32 +0100

    Added endpoints for replying and reacting to comments.

--- RespectCounter.API/Controllers/CommentController.cs
+++ RespectCounter.API/Controllers/CommentController.cs
@@ -1,13 +1,62 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using RespectCounter.Application.Commands;
 
 namespace RespectCounter.API.Controllers;
 
 [ApiController]
+[Route("api/comment")]
 public class CommentController: ControllerBase
 {
     private readonly ILogger<CommentController> logger;
-    public CommentController(ILogger<CommentController> logger)
+    private readonly ISender mediator;
+
+    public CommentController(ILogger<CommentController> logger, ISender mediator)
     {
         this.logger = logger;
+        this.mediator = mediator;
     }
+
+    #region Queries
+    
+    #endregion
+
+    #region Commands
+    [HttpPost("{id}")]
+    public async Task<IActionResult> CommentToComment(string id, [FromBody] string content)
+    {
+        var command = new AddCommentToParentCommentCommand(id, content, User);
+        try
+        {
+            var result = await mediator.Send(command);
+            
+            return Ok(result);
+        }
+        catch(System.Security.SecurityException)
+        {
+            return Unauthorized();
+        }
+    }
+  
+    [HttpPost("{id}/reaction/{reaction}")]
+    public async Task<IActionResult> ReactToComment(string id, int reaction)
+    {
+        var command = new AddReactionToCommentCommand(id, reaction, User);
+        var result = await mediator.Send(command);
+
+        return Ok(result);
+    }      
+
+    [HttpPut("{id}")]
+    public Task<IActionResult> UpdateComment(string id, [FromBody] string content)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id}/hide")]
+    public Task<IActionResult> HideComment(string id)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
 }
