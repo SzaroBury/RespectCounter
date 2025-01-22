9b406331804b121ecc0758ab6c10f1f50f0c4f7d
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 23:49:46 +0100

    Added CreateActivity page.

---
+++ RespectCounter.ReactApp/src/Pages/ActivitiesPage/CreateActivityPage/CreateActivityPage.js
@@ -0,0 +1,199 @@
+import "./CreateActivityPage.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import PersonDropdown from "./PersonDropdown/PersonDropdown";
+import TagDropdown from "./TagDropdown/TagDropdown";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../utils/AuthProvider/AuthProvider";
+
+function CreateActivityPage() {
+    const [ formData, setFormData ] = useState({ type: 1, personId: '' , title: '', description: '', happend: '', location: '', source: '', tags: '' });
+    const [ persons, setPersons ] = useState([]);
+    const [ personTags, setPersonTags ] = useState([]);
+    const { logout, openLoginPopup } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        loadPersons();
+    }, []);
+
+    const loadPersons = async () => {
+        console.log("CreateActivityPage: loadPersons()");
+        setPersons([]);
+    
+        axios.get('/api/persons/names')
+        .then(response => {
+            setPersons(response.data);
+        })
+        .catch(error => {
+            if (error.response) {
+                console.error(`HTTP error! Status: ${error.response.status}`);
+            } else if (error.request) {
+                console.error("No response received: ", error.request);
+            } else {
+                console.error("Error setting up the request: ", error.message);
+            }
+        });
+    }
+
+    const loadPersonTags = async (id) => {
+        console.log('CreateActivityPage.loadPersonTags(id:'+id+')');
+        setPersonTags([]);
+    
+        axios.get(`/api/person/${id}/tags`)
+        .then(response => {
+            setPersonTags(response.data);
+        })
+        .catch(error => {
+            if (error.response) {
+                console.error(`HTTP error! Status: ${error.response.status}`);
+            } else if (error.request) {
+                console.error("No response received: ", error.request);
+            } else {
+                console.error("Error setting up the request: ", error.message);
+            }
+        });
+    };
+
+    const handleDataChange = (e) => {
+        const { name, value } = e.target;
+        if(name === "type") {
+            setFormData({...formData, ["type"]: Number(value)})
+        } else if(name === "happend") {
+            setFormData({...formData, ["happend"]: new Date(value).toISOString()})
+        } else {
+            setFormData({...formData, [name]: value});
+        };
+    };
+
+    const handlePersonChange = (id) => {
+        console.log(`CreateActivityPage.handlePersonChange(id: ${id})`);
+        if(id)
+        {
+            setFormData({...formData, personId: id});
+            loadPersonTags(id);
+        } else {
+            setFormData({...formData, personId: ''});
+            setPersonTags([]);
+        }
+    };
+
+    const handleTagsChange = (selectedTags) => {
+        console.log(`CreateActivityPage.handleTagsChange(selectedTags[${selectedTags.length}])`);
+        if(selectedTags.length > 0)
+        {
+            setFormData({...formData, tags: selectedTags.map(t => t.name.toString()).join(',')});
+        } else {
+            setFormData({...formData, tags: ''});
+        }
+    };
+
+    const handleCreateButton = async () => {
+        try {
+            console.log('CreateActivityPage: handleCreateButton()', formData);
+            const response = await axios.post('/api/activity', formData);
+            console.log('CreateActivityPage: success ', response.data);
+            navigate(`/act/${response.data.id}`);
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                console.warn('Unauthorized! Redirecting to login...');
+                logout();
+                openLoginPopup();
+            } else {
+                console.error('An error occurred:', error);
+            }
+        }        
+    }
+
+    return (
+        <>
+            <div></div>
+            <div>
+                <h2 className="mt-5">Create a new activity</h2>
+                <span>Type<span className="text-danger">*</span>: </span>
+                <div className="input-group w-75 mb-3">
+                    <div className="mt-1">
+                        <div className="form-check form-check-inline">
+                            <input 
+                                id="inlineRadioCitation" 
+                                className="form-check-input" 
+                                type="radio" 
+                                name="type" 
+                                value = { Number(1) }
+                                onChange={handleDataChange}
+                                checked={ formData.type === 1 }
+                            />
+                            <label className="form-check-label" htmlFor="inlineRadioCitation">Quotation</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input 
+                                id="inlineRadioAct" 
+                                className="form-check-input" 
+                                type="radio" 
+                                name="type" 
+                                value = { Number(2) }
+                                onChange={handleDataChange} 
+                                checked={ formData.type === 2 } 
+                            />
+                            <label className="form-check-label" htmlFor="inlineRadioAct">Act</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="w-75 mb-3">
+                    <PersonDropdown title={"Person"} options={persons} onPersonChange={handlePersonChange}/>
+                </div>
+                <div className="input-group w-75 mb-3">
+                    <span className="input-group-text">
+                        { formData.type === 1 ? "Exact quotation" : "Title" }
+                        <span className="text-danger">*</span>
+                    </span>
+                    <input className="form-control" name="title" onChange={handleDataChange} required/>
+                </div>
+                <div className="input-group w-75 mb-3">
+                    <span className="input-group-text">
+                        Description
+                    </span>
+                    <textarea className="form-control" name="description" placeholder="" onChange={handleDataChange}/>
+                </div>
+                <div className="input-group w-75 mb-3">
+                    <span className="input-group-text">Happend<span className="text-danger">*</span>:</span>
+                    <input className="form-control" name="happend" type="date" onChange={handleDataChange}/>
+                </div>
+                { formData.type === 2 &&
+                    <div className="input-group w-75 mb-3">
+                        <span className="input-group-text">
+                            Location
+                        </span>
+                        <input className="form-control" name="location" placeholder="" onChange={handleDataChange}/>
+                    </div>
+                }
+                
+                <div className="input-group w-75 mb-3">
+                    <span className="input-group-text">
+                        Source
+                        <span className="text-danger">*</span>
+                    </span>
+                    <input className="form-control" name="source" placeholder="" onChange={handleDataChange} required/>
+                </div>
+                <span>Add some tags:</span>
+                <div className="w-75 mb-3">
+                    <TagDropdown personTags={personTags} onTagsChange={handleTagsChange} />
+                </div>
+                
+                <div className="mt-3 w-75 text-end">
+                    <span>
+                    Fields marked with <span className="text-danger">*</span> are required. 
+                    </span>
+                </div>
+                <div className="mt-3 w-75 text-end">
+                    <button className="btn btn-outline-primary" onClick={handleCreateButton}>Create</button>
+                </div>
+
+
+            </div>
+            <div></div>
+        </>
+    );
+}
+
+export default CreateActivityPage;
