d258443e230adf2e68e9b52bb094a4f471ac23b8
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 23:59:47 +0100

    Added displaying validation message. Catching 401 response. Refactoring logic of buttons that show passwords.

--- RespectCounter.ReactApp/src/components/LoginPopup/LoginPopup.js
+++ RespectCounter.ReactApp/src/components/LoginPopup/LoginPopup.js
@@ -1,10 +1,15 @@
+import "./LoginPopup.css";
 import { useState } from "react";
 import axios from 'axios';
-import "./LoginPopup.css";
+import { useAuth } from "../../utils/AuthProvider/AuthProvider";
+import { Link, useNavigate } from "react-router-dom";
 
-function LoginPopup({onCancel, onSuccessfullLoggingIn}) {
+function LoginPopup() {
     const [formData, setFormData] = useState({email: '', password: ''});
     const [showPassword, setShowPassword] = useState(false);
+    const [ validationMessage, setValidationMessage ] = useState("");
+    const { login, closeLoginPopup } = useAuth();
+    const navigate = useNavigate();
 
     const handleDataChange = (e) => {
         const { name, value } = e.target;
@@ -16,61 +21,72 @@ function LoginPopup({onCancel, onSuccessfullLoggingIn}) {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        // Obsługa logowania (np. wyślij dane do API)
-
         try {
             console.log('LoginPopup: sending login request:', formData);
+            setValidationMessage("");
             const response = await axios.post('/login', formData, { params: { useCookies: true } } );
             console.log('LoginPopup: success ', response.data);
-            onSuccessfullLoggingIn(formData.email)
+            login(formData.email);
+            closeLoginPopup();
         } catch (error) {
-            console.error('Error:', error);
-        }
-
-        console.log("Login submitted");
-        onCancel(); // Zamknij popup po zalogowaniu
+            if (error.response && error.response.status === 401) {
+                console.warn('Unauthorized! Redirecting to login...');
+                setValidationMessage("Invalid username or password. Please try again.");
+            } else {
+                console.error('An error occurred:', error);
+            }
+        }        
     };
+
+    const handleSignInClick = () => {
+        navigate("/signin");
+        closeLoginPopup();
+    }
 
     return (
         <div className="popup-backdrop">
-            <div className="popup-container">
+            <div className="popup-container border">
                 <h2>Log In</h2>
                 <div className="popup-cancel">
-                    <button type="button" className="btn btn-secondary" onClick={onCancel}><i class="bi bi-x-lg"></i></button>
+                    <button type="button" className="btn btn-outline-primary" onClick={closeLoginPopup}><i class="bi bi-x-lg"></i></button>
                 </div>
                 <form onSubmit={handleLogin}>
                     <div className="input-group mb-3">
-                        <input type="email" class="form-control" placeholder="Email" name="email" required
+                        <input type="text" className="form-control" placeholder="Email" name="email" required
                             onChange={handleDataChange}/>                        
                     </div>
                     <div className="input-group mb-3">
-                        {
-                            showPassword ? (
-                                <>
-                                    <input type="text" class="form-control" placeholder="Password" name="password" required
-                                        onChange={handleDataChange}/>
-                                    <button className="btn btn-secondary" type="button" id="button-addon2" onClick={() => setShowPassword(false)}><i class="bi bi-eye-slash"></i></button>
-                                </>
-                            ) : (
-                                <>
-                                    <input type="password" class="form-control" placeholder="Password" name="password" required
-                                        onChange={handleDataChange}/>
-                                    <button className="btn btn-secondary" type="button" id="button-addon2" onClick={() => setShowPassword(true)}><i class="bi bi-eye"></i></button>
-                                </>
-                            )
-                        }
+                        <input 
+                            type={showPassword ? "text" : "password"} 
+                            className="form-control" 
+                            placeholder="Password" 
+                            name="password" 
+                            required 
+                            onChange={handleDataChange} 
+                        />
+                        <button 
+                            className="btn btn-outline-primary" 
+                            type="button" 
+                            id="button-addon2" 
+                            onClick={() => setShowPassword((prev) => !prev)}
+                        >
+                            <i className={showPassword ? "bi bi-eye-slash" : "bi bi-eye"}></i>
+                        </button>
                     </div>
                     <div className="popup-actions">
-                        <button type="submit" className="btn btn-secondary">Log In</button>
+                        <button type="submit" className="btn btn-outline-primary">Log In</button>
+                    </div>
+                    <div className="text-center">
+                        <span className="text-danger">{validationMessage}</span>
                     </div>
                 </form>
-                <label className="popup-signin-label">If you don't have an account yet</label>
-                
                 <div className="popup-signin">
-                    <a href="/signin">
-                        <button className="btn btn-secondary" onClick={() => onCancel()}>Sign in</button>
-                    </a>
+                    <Link className="me-2" onClick={handleSignInClick}>
+                        Sign in
+                    </Link>
+                    <label className="popup-signin-label">if you don't have an account yet.</label>
                 </div>
+                
             </div>
         </div>
     );
