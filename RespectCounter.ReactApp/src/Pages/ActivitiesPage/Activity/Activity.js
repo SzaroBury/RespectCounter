66c1d8a060e529b9eb925865053fa630aa74f662
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 23:44:48 +0100

    Added Activity component.

---
+++ RespectCounter.ReactApp/src/Pages/ActivitiesPage/Activity/Activity.js
@@ -0,0 +1,52 @@
+import "./Activity.css";
+import { Link } from "react-router-dom";
+import ReactionButtons from "../../../components/ReactionButtons/ReactionButtons";
+import { useEffect } from "react";
+
+function Activity({a, showCommentsButton=true}) {
+    useEffect(() => {
+        console.log(a);
+    }, [])
+
+    return (
+        <div key={"activity_" + a.id} className='activity border'>
+            <div className='activity-header'>
+                <div className="d-flex">
+                    <div className='activity-person'>
+                        <img className='activity-person-image' src={`/persons/${a.personImagePath.toLowerCase()}`} alt={a.author}/>
+                    </div>
+                    <div className="activity-person-info">
+                        <span className="activity-person-name">{a.personFullName}</span>
+                        <span className='activity-person-respect'>{a.personRespect} respect</span>
+                    </div>
+                </div>
+                <span>{a.type === 1 ? ("Quotation") : ("Act")}</span>
+            </div>
+            <div className='activity-content'>
+                <p><strong>{a.value}</strong></p>
+                <p>{a.description}</p>
+                <span className='activity-source'>Source: {a.source}</span>
+            </div>
+            
+            <div className="activity-actions">
+                {showCommentsButton && <CommentsButton act={a} />}
+                <div className="activity-reaction-buttons">
+                    <ReactionButtons respect={a.respect} targetActivityId={a.id} />
+                </div>
+            </div>
+        </div>
+    )    
+}
+
+function CommentsButton({act}) {
+    return (
+        <Link className="btn btn-outline-primary p-2" to={"/act/" + String(act.id)}>
+            <span>
+                {act.commentsCount} {act.commentsCount === 1 ? "Comment" : "Comments"}
+            </span>
+            <i className="bi bi-chat-left m-2"></i>
+        </Link>
+    );
+};
+
+export default Activity;
