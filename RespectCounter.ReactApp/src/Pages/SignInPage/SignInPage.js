8b81f604d6e91cfe97b3809dfd93db2ba11b257d
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 23:55:42 +0100

    Added immediate login after successful account registration. Changed form to div. Updated button styles.

--- RespectCounter.ReactApp/src/Pages/SignInPage/SignInPage.js
+++ RespectCounter.ReactApp/src/Pages/SignInPage/SignInPage.js
@@ -1,21 +1,40 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import "./SignInPage.css";
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../utils/AuthProvider/AuthProvider';
 
 function SignInPage(props) {
-    const [formData, setFormData] = useState({email: '', password: ''});
+    const [formData, setFormData] = useState({ email: '', password: '' });
     const [showPassword, setShowPassword] = useState(false);
     const [showConfirmationPassword, setShowConfirmationPassword] = useState(false);
+    const navigate = useNavigate();
+    const { login } = useAuth();
+    
     const handleSignIn = async (e) => {
-        e.preventDefault(); // Zapobiega domyślnemu odświeżeniu strony
-
         try {
             const response = await axios.post('/register', formData);
-            console.log('Success:', response.data);
+            handleLogin();            
         } catch (error) {
             console.error('Error:', error);
         }
     };
+
+    const handleLogin = async () => {
+        try {
+            console.log('SignInPage: sending login request:', formData);
+            const response = await axios.post('/login', formData, { params: { useCookies: true } } );
+            console.log('SignInPage: success ', response.data);
+            login(formData.email);
+            navigate("/");
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                console.warn('Unauthorized! Redirecting to login...');
+            } else {
+                console.error('An error occurred:', error);
+            }
+        }        
+    }
 
     const handleDataChange = (e) => {
         const { name, value } = e.target;
@@ -28,8 +47,9 @@ function SignInPage(props) {
     return (
         <>
             <div className="signin">
-                <h2>Sign In</h2>
-                <form onSubmit={handleSignIn}>
+                <h2 className='signin-header'>Sign In</h2>
+                <div className='signin-form'>
+
                     <div className="input-group mb-3">
                         <input type="email" class="form-control" placeholder="Email" name="email" required 
                             onChange={handleDataChange}/>                        
@@ -40,7 +60,7 @@ function SignInPage(props) {
                                 <>
                                     <input type="text" class="form-control" placeholder="Password" name="password" required
                                         onChange={handleDataChange}/>
-                                    <button className="btn btn-secondary" type="button" id="button-addon2" 
+                                    <button className="btn btn-outline-primary" type="button" id="button-addon2" 
                                         onClick={() => setShowPassword(false)}>
                                         <i class="bi bi-eye-slash"></i>
                                     </button>
@@ -49,7 +69,7 @@ function SignInPage(props) {
                                 <>
                                     <input type="password" class="form-control" placeholder="Password" name="password" required
                                         onChange={handleDataChange}/>
-                                    <button className="btn btn-secondary" type="button" id="button-addon2" 
+                                    <button className="btn btn-outline-primary" type="button" id="button-addon2" 
                                         onClick={() => setShowPassword(true)}>
                                         <i class="bi bi-eye"></i>
                                     </button>
@@ -62,20 +82,20 @@ function SignInPage(props) {
                             showConfirmationPassword ? (
                                 <>
                                     <input type="text" class="form-control" placeholder="Confirm Password" name="confirm_password" required/>
-                                    <button className="btn btn-secondary" type="button" id="button-addon2" onClick={() => setShowConfirmationPassword(false)}><i class="bi bi-eye-slash"></i></button>
+                                    <button className="btn btn-outline-primary" type="button" id="button-addon2" onClick={() => setShowConfirmationPassword(false)}><i class="bi bi-eye-slash"></i></button>
                                 </>
                             ) : (
                                 <>
                                     <input type="password" class="form-control" placeholder="Confirm Password" name="confirm_password" required/>
-                                    <button className="btn btn-secondary" type="button" id="button-addon2" onClick={() => setShowConfirmationPassword(true)}><i class="bi bi-eye"></i></button>
+                                    <button className="btn btn-outline-primary" type="button" id="button-addon2" onClick={() => setShowConfirmationPassword(true)}><i class="bi bi-eye"></i></button>
                                 </>
                             )
                         }
                     </div>
                     <div className="popup-actions">
-                    <button className="btn btn-secondary" type="submit">Sign in</button>
+                        <button className="btn btn-outline-primary" onClick={handleSignIn}>Sign in</button>
                     </div>
-                </form>
+                </div>
             </div>
         </>
     );
