96c73687409939f166cb8de691f090f0124b0e61
SzaroBury <mkmateuszk@o2.pl>
2025-01-23 00:01:37 +0100

    Added working theme changing button. Changed style of buttons. Removed old code in comments.

--- RespectCounter.ReactApp/src/components/Header/Header.js
+++ RespectCounter.ReactApp/src/components/Header/Header.js
@@ -1,50 +1,57 @@
-import { useState } from 'react';
 import "./Header.css"
+import { useAuth } from '../../utils/AuthProvider/AuthProvider';
+import { useNavigate, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-function Header({onLoginClick, onLogoutClick, logged, user}) {
+function Header({onLoginClick}) {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const { isLoggedIn, user, logout, openLoginPopup } = useAuth();
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        // document.documentElement.setAttribute('data-theme', theme);
+        document.documentElement.setAttribute('data-bs-theme', theme);
+        localStorage.setItem('theme', theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        console.log("Header: toggleTheme()")
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
 
     return (
         <div className="header">
-            <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-                {/*<svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>*/}
-                <span className="fs-4">Respect Counter</span>
-            </a>
+            <div className="nav col-lg-auto me-lg-auto align-items-center">
+                <Link to="/" className="nav-link mb-2 mb-lg-0 text-decoration-none">
+                    <span className="fs-4">RespectCounter</span>
+                </Link>
+                <Link className="nav-link" to="/activities">Activities</Link>
+                <Link className="nav-link" to="/persons">Persons</Link>
+                <Link className="nav-link" to="/about">About</Link>
+            </div>
 
-            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="/events" className="nav-link link-dark">Events</a></li>
-                <li><a href="/persons" className="nav-link link-dark">Persons</a></li>
-                <li><a href="/about" className="nav-link link-dark">About</a></li>
-            </ul>
-
-            {/* <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-            <input type="search" className="form-control" placeholder="Search..." aria-label="Search"/ >
-            </form> */}
-
-            <div className="d-flex align-items-center gap-3">
-                <span><i className="bi bi-moon-stars-fill"></i></span>
-                {/* <div className="dropdown text-end">
-                    <a className="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle"/>
-                    </a>
-                    <ul className="dropdown-menu text-small" aria-labelledby="dropdownUser1">
-                        <li><a className="dropdown-item" href="/#">New project...</a></li>
-                        <li><a className="dropdown-item" href="/#">Settings</a></li>
-                        <li><a className="dropdown-item" href="/#">Profile</a></li>
-                        <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="/#">Sign out</a></li>
-                    </ul>
-                </div> */}
-
+            <div className="d-flex h-100 align-items-center">
+                <button className="btn me-3" onClick={toggleTheme}>
                 {
-                    logged ? (
-                        <div>
-                            <span className='m-3'>Hi {user}!</span>
-                            <button className='btn btn-secondary' onClick={onLogoutClick}>Log out</button>    
+                    theme === 'light' ? (
+                        <i className="bi bi-moon-stars-fill"></i> 
+                        
+                    ) : (
+                        <i className="bi bi-brightness-high-fill"></i>
+                    )
+                }
+                </button>
+                {
+                    isLoggedIn ? (
+                        <div className="d-flex h-100 align-items-center">
+                            <span className='me-3'>Hi {user}!</span>
+                            <button className='btn btn-outline-primary' onClick={logout}>Log out</button>    
                         </div>  
                         
                     ) : (
                         <div>
-                            <button className='btn btn-secondary' onClick={onLoginClick}>Log in</button>
+                            <button className='btn btn-outline-primary' onClick={openLoginPopup}>Log in</button>
                         </div>
                     )
                 }
