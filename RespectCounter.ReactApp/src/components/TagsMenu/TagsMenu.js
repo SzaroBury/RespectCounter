6e11d066df02381b4004a5b94984b53c64cf398f
SzaroBury <mkmateuszk@o2.pl>
2025-01-23 00:08:03 +0100

    Added showing placeholders while loading data. Added logic for selecting tags.

--- RespectCounter.ReactApp/src/components/TagsMenu/TagsMenu.js
+++ RespectCounter.ReactApp/src/components/TagsMenu/TagsMenu.js
@@ -1,17 +1,43 @@
-import React, { useState, useEffect } from 'react';
 import './TagsMenu.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function TagsMenu({onTagSelected}) {
+function TagsMenu({countMode = 'count', tagsSelected, setTagsSelected, currentTags}) {
+
+    const handleTagClick = (tag) => {
+        if(tagsSelected.some(ts => ts.id === tag.id)) {
+            setTagsSelected(tagsSelected.filter(ts => ts.id !== tag.id));
+        } else {
+            setTagsSelected([...tagsSelected, tag]);
+        };
+    };
+
     return (
         <div className="tags-menu">
             <section>
                 <h5 className='m-3'>Tags</h5> 
             </section>
-            <RecentBrowsedTags />
-            <FavouriteTags />
-            <TrendingTags />
-            <AllTags onTagSelection={onTagSelected}/>
+            {currentTags && <CurrentTags currentTags={currentTags}/>}
+            <RecentBrowsedTags tagsSelected={tagsSelected} />
+            <FavouriteTags tagsSelected={tagsSelected} />
+            <TrendingTags tagsSelected={tagsSelected}/>
+            <AllTags count={countMode} tagsSelected={tagsSelected} onTagClick={handleTagClick}/>
         </div>
+    );
+}
+
+function CurrentTags({currentTags}) {
+    return (
+        currentTags && (
+            <section>
+                <h6>Current tags:</h6>
+                <ul>
+                    {currentTags.map((tag) => (
+                        <li key={"CurrentTag_" + tag}>{tag}</li>
+                    ))}
+                </ul>
+            </section>
+        )
     );
 }
 
@@ -25,15 +51,34 @@ function RecentBrowsedTags() {
     const [tags, setTags] = useState([]);
 
     useEffect(() => {
-        console.log("Fetching recent browsed tags...");
-        fetch('/api/tags/recent') // to do on API side
-            .then(response => response.json())
-            .then(data => setTags(data))
-            .catch(error => console.error("Error fetching recent tags:", error));
-    }, []); // Empty dependency array ensures this runs only once, like `componentDidMount`
+        loadTags();
+    }, []); 
+
+    const loadTags = () => {
+        console.log("TagsMenu.RecentBrowsedTags: loadTags()");
+        axios.get('/api/tags/recent')
+            .then(response => {
+                setTags(response.data);
+            })
+            .catch(error => {
+                if (error.response) 
+                {
+                    console.error(`HTTP error! Status: ${error.response.status}`);
+                } 
+                else if (error.request) 
+                {
+                    console.error("No response received: ", error.request);
+                } 
+                else 
+                {
+                    console.error("Error setting up the request: ", error.message);
+                }
+            }
+        );
+    }
 
     if (tags.length === 0) {
-        return null; // Do not render the section if no tags are available
+        return null;
     }
 
     return (
@@ -41,7 +86,7 @@ function RecentBrowsedTags() {
             <h6>Recent browsed:</h6>
             <ul>
                 {tags.map((tag) => (
-                    <li key={"RecentTag_" + tag}>{tag.name} ({tag.count})</li>
+                    <li key={"RecentTag_" + tag.id}>{tag.name} ({tag.count})</li>
                 ))}
             </ul>
         </section>
@@ -52,12 +97,31 @@ function FavouriteTags() {
     const [tags, setTags] = useState([]);
 
     useEffect(() => {
-        console.log("Fetching favourite tags...");
-        fetch('/api/tags/favourite') // todo on API side
-            .then(response => response.json())
-            .then(data => setTags(data))
-            .catch(error => console.error("Error fetching favourite tags:", error));
+        loadTags();
     }, []);
+
+    const loadTags = () => {
+        console.log("TagsMenu.FavouriteTags: loadTags()");
+        axios.get('/api/tags/favourite')
+        .then(response => {
+            setTags(response.data);
+        })
+        .catch(error => {
+            if (error.response) 
+            {
+                console.error(`HTTP error! Status: ${error.response.status}`);
+            } 
+            else if (error.request) 
+            {
+                console.error("No response received: ", error.request);
+            } 
+            else 
+            {
+                console.error("Error setting up the request: ", error.message);
+            }
+        }
+    );
+    }
 
     if (tags.length === 0) {
         return null;
@@ -68,57 +132,72 @@ function FavouriteTags() {
             <h6>Favourite:</h6>
             <ul>
                 {tags.map((tag) => (
-                    <li key={"FavouriteTag_" + tag}>{tag.name} ({tag.count})</li>
+                    <li key={"FavouriteTag_" + tag.name}>{tag.name} ({tag.count})</li>
                 ))}
             </ul>
         </section>
     );
 }
 
-function AllTags({onTagSelection}) {
-    const [tags, setTags] = useState(["loading..."]);
-    const [selectedTagIndex, setSelectedTagIndex] = useState(null);
-    const onTagClick = (tags, index) => {
-        console.log("   AllTags: onTagClick(" + index + ")");
-        onTagSelection(tags[index].name);
-        setSelectedTagIndex(index); 
-    };
+function AllTags({count, tagsSelected, onTagClick}) {
+    const [tags, setTags] = useState([]);
 
     useEffect(() => {
-        console.log("Fetching all tags...");
-        fetch('/api/tags') // Replace with your actual API endpoint
-            .then(response => response.json())
-            .then(data => { 
-                setTags(data); 
-                console.log(data);
-            })
-            .catch(error => console.error("Error fetching all tags:", error));
+        loadTags();
     }, []);
 
-    if (tags.length === 0) {
-        return null;
+    const loadTags = () => {
+        console.log("TagsMenu.AllTags: loadTags()");
+        axios.get('/api/tags')
+            .then(response => {
+                setTags(response.data);
+            })
+            .catch(error => {
+                if (error.response) 
+                {
+                    console.error(`HTTP error! Status: ${error.response.status}`);
+                } 
+                else if (error.request) 
+                {
+                    console.error("No response received: ", error.request);
+                } 
+                else 
+                {
+                    console.error("Error setting up the request: ", error.message);
+                }
+            }
+        );
     }
-    
 
     return (
-        <section>
-            <h6>All:</h6> 
-            <ul>
-                {tags.map((tag, index) => (
-                    <li key={"Tag_" + index} 
-                        className={selectedTagIndex === index  ? "selected-tag" : ""}
-                        onClick={() => onTagClick(tags, index)}
-                    >
-                        {tag.name} ({tag.count})
+    <section>
+        <h6>All tags:</h6>
+
+        {tags.length === 0 ? (
+            <ul className="placeholder-glow">
+                {Array.from({ length: 10 }, (_, index) => (
+                    <li key={`placeholder_${index}`} className="placeholder">
+                        Placeholder
                     </li>
                 ))}
             </ul>
-        </section>
+        ) : (
+            <ul>
+                {tags
+                    .filter(tag => tag[count] > 0)
+                    .map(tag => (
+                        <li
+                            key={`Tag_${tag.id}`}
+                            className={`tag-item ${tagsSelected.some(ts => ts.id === tag.id) ? "selected-tag" : ""}`}
+                            onClick={() => onTagClick(tag)}
+                        >
+                            {tag.name} ({tag[count]})
+                        </li>
+                    ))}
+            </ul>
+        )}
+    </section>
     );
-}
-
-function loadTags() {
-
 }
 
 export default TagsMenu;
