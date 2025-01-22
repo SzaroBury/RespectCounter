26f0a1c8a38634861bda1f41761a31163cca69f2
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 23:43:03 +0100

    Added Footer component.

---
+++ RespectCounter.ReactApp/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer() {
+    return (
+    <div className="footer">
+        <div>Made by Mateusz Kubicki</div>
+    </div>
+    );
+}
+
+export default Footer;
