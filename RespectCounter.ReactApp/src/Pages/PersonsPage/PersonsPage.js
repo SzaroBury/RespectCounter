20caf08e1b9c197522d8a937a47a6bda8ac411c4
SzaroBury <mkmateuszk@o2.pl>
2025-01-23 00:15:11 +0100

    Refactoring whole component from class to function. Added SortMenu, TagsMenu and Person components. Added list showing people existing in the system.

--- RespectCounter.ReactApp/src/Pages/PersonsPage/PersonsPage.js
+++ RespectCounter.ReactApp/src/Pages/PersonsPage/PersonsPage.js
@@ -1,23 +1,104 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container'
+import './PersonsPage.css';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import SortMenu from '../../components/SortMenu/SortMenu';
+import TagsMenu from '../../components/TagsMenu/TagsMenu';
+import Loading from '../../components/Loading/Loading';
+import axios from 'axios';
+import Person from './Person/Person';
+import { useAuth } from '../../utils/AuthProvider/AuthProvider';
 
-class PersonsPage extends React.Component {
-    static displayName = "RespectCounter - Persons";
+function PersonsPage({props}) {
+    const [loading, setLoading] = useState(true);
+    const [title, setTitle] = useState('Persons - Most Respected');
+    const [persons, setPersons] = useState([]);
+    const [sortOption, setSortOption] = useState("mr");
+    const [scopeOption, setScopeOption] = useState(true);
+    const [tagsSelected, setTagsSelected] = useState([]);
+    const navigate = useNavigate();
+    const {isLoggedIn, openLoginPopup} = useAuth();
 
-    constructor(props) {
-        super(props);
-        this.state = { loading: true };
+    useEffect(() => {
+        setLoading(true);
+        loadPersons(sortOption, scopeOption);
+    }, [sortOption, scopeOption]);
+
+    const handleSortChange = (sortOptionName, sortOptionDiplay) => {
+        console.log("PersonsPage: handleSortChange(" + sortOptionName + ", " + sortOptionDiplay + ")")
+        setTitle("Persons - " + sortOptionDiplay);
+        setSortOption(sortOptionName);
+    };
+    
+    const handleScopeChange = () => {
+        console.log("PersonsPage: handleScopeChange()");
+        const targetValue = !scopeOption;
+        setScopeOption(targetValue);
+    };
+    const handleTagSelected = () => {
+        console.log('PersonsPage: handleTagSelected()')
     }
 
-    render() {
-        return (
-            <Container>
-                <SortMenu page="Persons"/>
-                <h2>Tag name</h2>
-            </Container>
-        );
+    const handleCreateButton = () => {
+        console.log('PersonsPage: handleCreateButton()');
+        if(isLoggedIn)
+        {
+            navigate('/person/create');
+        }
+        else
+        {
+            openLoginPopup();
+        }
     }
+
+    const loadPersons = (sortOption, onlyVerified) => {
+        console.log('PersonsPage: loadPersons(sortOption: "' + sortOption + '", onlyVerified: "' + onlyVerified + '")')
+        setPersons([]);
+    
+        const ifVerifiedUrl = onlyVerified ? "" : "/all";
+    
+        axios.get(`/api/persons${ifVerifiedUrl}`, {
+            params: {
+                order: sortOption
+            }
+        })
+        .then(response => {
+            setPersons(response.data);
+            setLoading(false);
+        })
+        .catch(error => {
+            setLoading(false);
+
+            if (error.response) {
+                console.error(`HTTP error! Status: ${error.response.status}`);
+            } else if (error.request) {
+                console.error("No response received: ", error.request);
+            } else {
+                console.error("Error setting up the request: ", error.message);
+            }
+        });
+    }
+
+    return (
+        <>
+            <SortMenu page="Persons" onSortOptionChange={handleSortChange} onScopeChange={handleScopeChange}/>
+            <div></div>
+            <div>
+                <div className='text-end'>
+                    <button className='btn btn-outline-primary' title="Propose a public figure" onClick={handleCreateButton}>
+                        <span className='me-2'>Propose public figure</span> 
+                        <i className="bi bi-person-plus-fill"></i>
+                    </button>
+                </div>
+                <h2>{title}</h2>
+                <Loading loading={loading}/>
+                {persons.map((per, index) => 
+                    <Person key={"Person_" + per.id} person={per} index={index}/>
+                )}
+            </div>
+            <div></div>
+            <TagsMenu countMode="countPersons" tagsSelected={tagsSelected} setTagsSelected={setTagsSelected}/>
+        </>
+    );
 }
 
 export default PersonsPage;
