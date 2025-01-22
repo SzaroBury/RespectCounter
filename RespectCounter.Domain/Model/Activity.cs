3be2e132947f3292b5d18e229b562b97f367926c
SzaroBury <mkmateuszk@o2.pl>
2025-01-22 14:34:14 +0100

    "Happend" field was set as nullable. Renamed one of the possible options in the ActivityType enum (Event -> Act).

--- RespectCounter.Domain/Model/Activity.cs
+++ RespectCounter.Domain/Model/Activity.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RespectCounter.Domain.Model
 {
@@ -8,13 +7,13 @@ namespace RespectCounter.Domain.Model
         public string Value { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
-        public DateTime Happend { get; set; } = DateTime.MinValue;
+        public DateTime? Happend { get; set; } = DateTime.MinValue;
         public string Source { get; set; } = string.Empty;
         public ActivityStatus Status { get; set; } = ActivityStatus.NotVerified;
-        public ActivityType Type { get; set; } = ActivityType.Event;
+        public ActivityType Type { get; set; } = ActivityType.Act;
         public Guid PersonId { get; set; }
         [Required]
-        public virtual Person Person { get; set; }
+        public virtual Person? Person { get; set; }
         public virtual List<Reaction> Reactions { get; set; } = new();
         public virtual List<Comment> Comments { get; set; } = new();
         public virtual List<Tag> Tags { get; set; } = new();
@@ -22,7 +21,7 @@ namespace RespectCounter.Domain.Model
 
     public enum ActivityType
     {
-        Event,
+        Act,
         Quote
     }
 
