386dcea6093d525715b73b232619901377a3a7ee
SzaroBury <mkmateuszk@o2.pl>
2025-01-29 13:50:34 +0100

    Added JWT authentication configuration with issuer, audience, and secret key from appsettings.json. Added JWT token handling in cookies for client-side usage. Enabled cookie policy to enforce secure and HttpOnly cookies for added security. Updated the application services with IJwtService. Switched to dynamic database configuration using "DB" setting from appsettings.json (InMemory or SQL Server).

--- RespectCounter.API/Program.cs
+++ RespectCounter.API/Program.cs
@@ -1,12 +1,14 @@
 using System.Text;
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using RespectCounter.Infrastructure;
 using Microsoft.AspNetCore.Identity;
+using RespectCounter.Domain.Interfaces;
+using RespectCounter.Infrastructure;
 using RespectCounter.Infrastructure.Repositories;
+using RespectCounter.Infrastructure.Services;
 using RespectCounter.Application;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,42 +17,64 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "DevPolicy", policy  =>
     {
-        policy.WithOrigins("http://localhost:3000");
+        policy.WithOrigins("http://localhost:3000", "https://localhost:3000");
     });
 });
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.HttpOnly = Microsoft.AspNetCore.CookiePolicy.HttpOnlyPolicy.Always;
+    options.Secure = CookieSecurePolicy.Always;
+});
+
 builder.Services.AddControllers().AddJsonOptions(o => o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); //to prevent reference cycle error
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-    .AddEntityFrameworkStores<RespectDbContext>()
-    .AddApiEndpoints();
-if(args.Any(arg => arg == "-m"))
+
+if(builder.Configuration["DB"] == "InMemory")
 {
     builder.Services.AddDbContext<RespectDbContext>(options => options.UseInMemoryDatabase("RespectCounterDB"));
     Console.WriteLine("The server is going to use an in-memory database.");
 }
 else builder.Services.AddDbContext<RespectDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<RespectDbContext>();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(RespectService)));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// to-do
-// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-//     .AddJwtBearer(options =>
-//     {
-//         options.TokenValidationParameters = new TokenValidationParameters()
-//         {
-//             ClockSkew = TimeSpan.Zero,
-//             ValidateIssuer = true,
-//             ValidateAudience = true,
-//             ValidateLifetime = true,
-//             ValidateIssuerSigningKey = true,
-//             ValidIssuer = "apiWithAuthBackend",
-//             ValidAudience = "apiWithAuthBackend",
-//             IssuerSigningKey = new SymmetricSecurityKey(
-//                 Encoding.UTF8.GetBytes("!SomethingSecret!")
-//             ),
-//         };
-//     });
+
+builder.Services.AddAuthentication(options => 
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"] ?? "")
+            ),
+        };
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                context.Token = context.Request.Cookies["jwt"];
+                return Task.CompletedTask;
+            }
+        };
+    }
+);
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -61,11 +85,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCookiePolicy();
 app.UseHttpsRedirection();
 app.UseCors("DevPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
-app.MapIdentityApi<IdentityUser>();
 
 app.Run();
