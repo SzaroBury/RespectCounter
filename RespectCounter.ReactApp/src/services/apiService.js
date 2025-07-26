9c494ca99f3493e717263a3ed334b95a082b7951
SzaroBury <mkmateuszk@o2.pl>
2025-07-26 09:28:48 +0200

    Deleted a redundant service

--- RespectCounter.ReactApp/src/services/apiService.js
+++
@@ -1,7 +0,0 @@
-import api from './apiService';
-
-export const getPersons = (params) => api.get('/persons', { params });
-export const getPersonById = (id) => api.get(`/persons/${id}`);
-export const addPerson = (data) => api.post('/persons', data);
-export const updatePerson = (id, data) => api.put(`/persons/${id}`, data);
-export const deletePerson = (id) => api.delete(`/persons/${id}`);
